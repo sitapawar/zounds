0881ca4092b08fd12b429b290c9b0e6b90b00963
sitapawar <sita_pawar@brown.edu>
2025-07-01 12:54:23 -0400

    more responsive and data tracking

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './style.css';
 import logo from '../mylogo.png';
 
-export default function Navbar({ onHomeClick, onStartOverClick, onAboutClick }) {
+export default function Navbar({ onHomeClick, onAboutClick, onResetClick }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  // Collapse menu on outside click
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setMenuOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
   return (
-    <nav className="navbar">
-      <div className="navbar-left" onClick={onHomeClick} style={{ cursor: 'pointer' }}>
-        <img src={logo} alt="Logo" className="navbar-logo" />
-        <span className="navbar-title">zounds.</span>
+    <nav className="navbar" ref={menuRef}>
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="navbar-logo" onClick={onHomeClick} />
+        <span className="navbar-title" onClick={onHomeClick}>zounds.</span>
       </div>
 
-      <div className="navbar-right">
-        <button className="navbar-button" onClick={onHomeClick}>Start Over</button>
-        <button className="navbar-button" onClick={onAboutClick}>About</button>
-
+      <div className={`navbar-menu-toggle ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+        <div></div><div></div><div></div>
       </div>
+
+      <ul className={`navbar-links ${menuOpen ? 'show' : ''}`}>
+        <li onClick={onHomeClick}>Home</li>
+        <li onClick={onAboutClick}>About</li>
+      </ul>
     </nav>
   );
 }
